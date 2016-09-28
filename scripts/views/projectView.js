4e6d3f724be92a21a5f4f646636be4bfa661d807
dratech <alfonsotech@nym.hush.com>
2016-09-28 00:32:25 -0700

    finished adding methods to the articleView object; moved animate header function onto arcileView object; called all three methods

--- scripts/views/projectView.js
+++ scripts/views/projectView.js
@@ -1,6 +1,9 @@
-//***VIEW***
+//empty object to attach view handlers
+var projectView = {};
+
+
 //Set navigation so that only section selected shows, beginning with the "About" section
-projectArray.handleNavTabs = function () {
+projectView.handleNavTabs = function () {
   //click event handler for navigation tabs
   $('.main-nav').on('click', '.navtab', function () {
     //store this object reference in a variable
@@ -24,19 +27,34 @@ projects.sort(function(this1, this2) {
   return (new Date(this2.publishedOn)) - (new Date(this1.publishedOn));
 });
 
-//push built/compiled project entries into empty projectArray
-projects.forEach(function(entry) {
-  projectArray.push(new Project(entry));
-});
+//put all render code into a function that can be called
+projectView.renderIndexPage = function() {
+  //push built/compiled project entries into empty projectArray
+  //????IS this still okay as is???????<=======================================
+  Project.all.forEach(function(entry) {
+    projectArray.push(new Project(entry));
+  });
 
-//render to HTML
-projectArray.forEach(function(item){
-  $('#project').append(item.toHtml());
-});
+  //render to HTML
+  projectArray.forEach(function(item){
+    $('#project').append(item.toHtml());
+  });
+};
 
 /*Helper Functions*/
-//animate header
-function animateHeader (){
+//animate header - not sure what to do with this helper function that I added on my own - it's currently floating. I shoudl probably add this as a method on the projectView object????????
+//Old Code
+// function animateHeader (){
+//   $('h1').on('mouseenter', function(){
+//     $('h1').text('.τέχνη(ἰδέα);');
+//     $('h2').text('Sturdy Technology From Ideas');
+//   });
+//   $('h1').on('mouseout', function(){
+//     $('h1').text('A L F ◯ N S O');
+//     $('h2').text('Web Development');
+//   });
+// }
+projectView.animateHeader = function() {
   $('h1').on('mouseenter', function(){
     $('h1').text('.τέχνη(ἰδέα);');
     $('h2').text('Sturdy Technology From Ideas');
@@ -45,10 +63,10 @@ function animateHeader (){
     $('h1').text('A L F ◯ N S O');
     $('h2').text('Web Development');
   });
-}
+};
 
 /*Function Calls*/
-//call the function to handle nav tabs
-projectArray.handleNavTabs();
-//animate header
-animateHeader ();
+//call all the methods on the projectView object
+projectView.handleNavTabs();
+projectView.animateHeader();
+projectView.renderIndexPage();
