9f38f4011cb0add3e1282317398b2df8b46e91a2
dratech <alfonsotech@nym.hush.com>
2016-09-27 23:29:18 -0700

    added new file structure to include models and views; added files inside views for newProject and projectView

---
+++ scripts/models/projectsArray.js
@@ -0,0 +1,30 @@
+'use strict';
+var projects = [
+  {
+    title: 'Continental Philosophy Tube',
+    githubUrl: 'https://github.com/alfonsotech/Continental-Phil-Tube',
+    deployedUrl: 'https://alfonsotech.github.io/Continental-Phil-Tube',
+    skills: 'HTML, CSS, Bootstrap, Javascipt, jQuery',
+    projectImage: 'images/cptube.png',
+    description:'',
+    dateCreated:'2016-09-07'
+  },
+  {
+    title: 'Continental Philosophy Quiz',
+    githubUrl: 'https://github.com/alfonsotech/philosophy-quiz-app',
+    deployedUrl: 'https://alfonsotech.github.io/philosophy-quiz-app/',
+    skills: 'HTML, CSS, Javascipt, jQuery',
+    projectImage: 'images/cpquiz.png',
+    description:'',
+    dateCreated:'2015-09-06'
+  },
+  {
+    title: 'Pomodoro Clock',
+    githubUrl: 'https://github.com/alfonsotech/pomodoro-clock',
+    deployedUrl: 'https://alfonsotech.github.io/pomodoro-clock/',
+    skills: 'HTML, CSS, Bootstrap, Javascipt, jQuery',
+    projectImage: 'images/pomodoro-clock3.png',
+    description:'',
+    dateCreated:'2014-12-05'
+  }
+];
